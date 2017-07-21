66b44d3de0964667c357786b7f5033a6f3fc8aa4
yuvaranis <30317662+yuvaranis@users.noreply.github.com>
2017-07-21 17:27:48 +0530

    Create sun.java

---
+++ sun.java
@@ -0,0 +1,15 @@
+    package com.candidjava.code;
+     
+    class SumOfNum
+    {
+    	public static void main(String args[])
+    	{
+    		int sum = 0;
+    		int n=10;
+    		for(int i = 1;i <= n;i++)
+    		{
+    			sum = sum + i;
+    		}
+    		System.out.println("The Sum Of "+n+" Numbers are:" + sum);
+    	}
+    }
