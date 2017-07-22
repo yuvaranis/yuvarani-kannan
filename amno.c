addccced3bf6bbdf94447abf0305358b6db605da
yuvaranis <30317662+yuvaranis@users.noreply.github.com>
2017-07-22 18:06:25 +0530

    Update amno.c

--- amno.c
+++ amno.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <math.h>
- 
-int main()
+ int main()
 {
     int originalNum, num, lastDigit, digits, sum;
         printf("Enter any number to check Armstrong number: ");
