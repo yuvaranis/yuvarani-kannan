3e265251d37d8698a4c1384ed09d055b3931d75b
yuvaranis <30317662+yuvaranis@users.noreply.github.com>
2017-07-22 17:52:21 +0530

    Update multitable.c

--- multitable.c
+++ multitable.c
@@ -2,14 +2,11 @@
 int main()
 {
     int n, i;
-
     printf("Enter an integer: ");
     scanf("%d",&n);
-
     for(i=1; i<=10; ++i)
     {
         printf("%d * %d = %d \n", n, i, n*i);
     }
-    
-    return 0;
+       return 0;
 }
