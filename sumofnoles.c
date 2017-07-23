e94d3e4d322ab157367fe532ca038765364b4cee
yuvaranis <30317662+yuvaranis@users.noreply.github.com>
2017-07-23 11:58:01 +0530

    Update sumofnoles.c

--- sumofnoles.c
+++ sumofnoles.c
@@ -1,15 +1,19 @@
+ 
+#include<stdio.h>
 #include<conio.h>
 void main()
 {
-    int i,sum = 0;
-    clrscr();
-
-    for(i = 100;i <= 200 ; i++)
-    {
-        if (i % 7 == 0)
-        sum = sum + i;
-    }
-    printf("\n Sum of all no between 100 and 200 ");
-    printf("which is divisible by 7 is :: %d",sum);
-    getch();
+   int i, sum=0;
+   clrscr();
+   printf("All nos. between 100 - 200 which is divisible by 7\n");
+   for(i=101;i<200;i++)
+   {
+     if(i%7==0)
+     {
+       printf("%5d",i);
+       sum+=i;
+     }
+   }
+   printf("\n\nsum = %d",sum);
+   getch();
 }
