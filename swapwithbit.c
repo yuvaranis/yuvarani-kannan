7fab90cc2d3fe9e803ac1a979964f89771bea781
yuvaranis <30317662+yuvaranis@users.noreply.github.com>
2017-07-23 13:34:11 +0530

    Update swapwithbit.c

--- swapwithbit.c
+++ swapwithbit.c
@@ -10,5 +10,15 @@ void main()
     swap(&num1, &num2);     
     printf("\nThe numbers after swapping are Number1= %d Number2 = %d", num1, num2);
 }
+void swap(int *x, int *y)
 
+{
+
+    *x = *x ^ *y;
+
+    *y = *x ^ *y;
+
+    *x = *x ^ *y;
+
+}
  
