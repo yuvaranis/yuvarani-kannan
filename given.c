9c48ed9b4d8b34ec242a512ce66880f5a8d0e2f3
yuvaranis <30317662+yuvaranis@users.noreply.github.com>
2017-07-24 12:20:30 +0530

    Update given.c

--- given.c
+++ given.c
@@ -1,33 +1,15 @@
 #include <stdio.h>
-
 #include <string.h>
-
- 
-
 void main()
-
 {
-
     char s[200];
-
     int count = 0, i;
-
- 
-
-    printf("enter the string\n");
-
+     printf("enter the string\n");
     scanf("%[^\n]s", s);
-
     for (i = 0;s[i] != '\0';i++)
-
     {
-
         if (s[i] == ' ')
-
-            count++;    
-
+           count++;    
     }
-
     printf("number of words in given string are: %d\n", count + 1);
-
 }
