3f353eea235b461b5ebbbe4f4e3c1020075910ef
yuvaranis <30317662+yuvaranis@users.noreply.github.com>
2017-07-27 13:40:34 +0530

    Create mins.c

---
+++ mins.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h> // for abs()
+#include <limits.h> // for INT_MAX
+ int minDist(int arr[], int n, int x, int y)
+{
+   int i, j;
+   int min_dist = INT_MAX;
+   for (i = 0; i < n; i++)
+   {
+     for (j = i+1; j < n; j++)
+     {
+         if( (x == arr[i] && y == arr[j] ||
+              y == arr[i] && x == arr[j]) && min_dist > abs(i-j))
+         {
+              min_dist = abs(i-j);
+         }
+     }
+   }
+   return min_dist;
+}
+int main() 
+{
+    int arr[] = {3, 5, 4, 2, 6, 5, 6, 6, 5, 4, 8, 3};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int x = 3;
+    int y = 6;
+     printf("Minimum distance between %d and %d is %d\n", x, y, 
+              minDist(arr, n, x, y));
+    return 0;
+}
